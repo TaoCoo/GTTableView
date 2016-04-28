7ad0efe2ee510a8a098aef211702ec20c8a59333
gt <gt@liquandeiMac.local>
2016-04-28 10:31:48 -0700

    init

---
+++ GTTableView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GTTableView
+//
+//  Created by gt on 16/4/28.
+//  Copyright © 2016年 personal.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GTTableView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GTTableView
+//
+//  Created by gt on 16/4/28.
+//  Copyright © 2016年 personal.com. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
